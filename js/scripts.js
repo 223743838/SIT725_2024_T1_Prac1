c0bbeaeac6eab35ca57af93f6b50c4ccd6b67c8d
s223743838 <s223743838@deakin.edu.au>
2024-03-09 17:21:33 +1100

    Workshop-1

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function changeText(){
+
+    var textsArray = ["Aws","Azure", "GCP", "Html", "JavaScript","CSS"]
+
+    var number = getRandomNumberBetween(0,textsArray.length - 1)
+
+    console.log("Index: ", number)
+
+    document.getElementById("heading").innerHTML = textsArray[number];
+
+}
+
+function getRandomNumberBetween(min,max){
+
+    return Math.floor(Math.random()*(max-min+1)+min);
+
+}
